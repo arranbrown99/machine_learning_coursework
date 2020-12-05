2ed509c65ea617fb9d713ae01d1dcb2d9e04c71e
ArranBrown <2323915b@student.gla.ac.uk>
2020-12-05 12:01:32 +0000

    precision and recall

--- uofg-mlh-2021-c/helper_functions.py
+++ uofg-mlh-2021-c/helper_functions.py
@@ -1,6 +1,7 @@
-from sklearn.metrics import accuracy_score, roc_auc_score, recall_score, confusion_matrix, roc_curve
-from matplotlib.ticker import MultipleLocator
+from sklearn.metrics import accuracy_score, roc_auc_score, recall_score,precision_score,f1_score
+from sklearn.metrics import confusion_matrix, roc_curve,precision_recall_curve
 
+from matplotlib.ticker import MultipleLocator
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -8,9 +9,14 @@ from matplotlib import pyplot as plt
 def evaluate(predicted_y_values, actual_y_values):
     accuracy = accuracy_score(actual_y_values, predicted_y_values)
     sensitivity = recall_score(actual_y_values, predicted_y_values)
+    precision = precision_score(actual_y_values, predicted_y_values)
+    
+    f1 = f1_score(actual_y_values, predicted_y_values)
     
     print("Accuracy Rate = " + str(accuracy))
-    print("Sensitivity Rate = " + str(sensitivity))
+    print("Sensitivity\Recall Rate = " + str(sensitivity))
+    print("Precision Rate = " + str(precision))
+    print("F1 Score = " + str(f1))
     
     
     con_matrix = confusion_matrix(actual_y_values, predicted_y_values)
@@ -19,15 +25,28 @@ def evaluate(predicted_y_values, actual_y_values):
 
 
 def plot_roc(actual_y_values,predicted_validation_y_probs):
+    # good when each class has roughly equal number of entries
     roc_auc_score_value = roc_auc_score(actual_y_values, predicted_validation_y_probs)
     print("ROC AUC Score= " + str(roc_auc_score_value))
     
-    roc_y, roc_x, thresholds = roc_curve(actual_y_values, predicted_validation_y_probs,pos_label = 1)
-    plt.plot(roc_x, roc_y, label='Logistic')
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
+    false_positive_rate, true_positive_rate, thresholds = roc_curve(actual_y_values, predicted_validation_y_probs,pos_label = 1)
+    
+    #plt.plot(false_positive_rate, true_positive_rate, label='Logistic')
+    plt.plot(true_positive_rate,false_positive_rate)
+    plt.xlabel('True Positive Rate')
+    plt.ylabel('False Positive Rate')
+    
 
-
+def plot_precision_recall(actual_y_values,predicted_validation_y_probs):
+    #deals with class imbalance
+    precision, recall,thresholds = precision_recall_curve(actual_y_values,predicted_validation_y_probs,pos_label = 1)
+ 
+    plt.plot(recall, precision)
+    
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    
+    
 def plot_confusion_matrix(con_matrix,labels,text_x=-0.2,text_y=-1):
     print("Confusion Matrix of Classes")
     print(con_matrix)
@@ -37,7 +56,7 @@ def plot_confusion_matrix(con_matrix,labels,text_x=-0.2,text_y=-1):
     ax = fig.add_subplot(111)
     cax = ax.matshow(con_matrix)
     plt.text(text_x,text_y, 'Confusion matrix of the classifier')
-    #plt.title('Confusion matrix of the classifier',loc = 'left')
+    
     fig.colorbar(cax)
     ax.set_xticklabels([''] + labels)
     ax.set_yticklabels([''] + labels)
